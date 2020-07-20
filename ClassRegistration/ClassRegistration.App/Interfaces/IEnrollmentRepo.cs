85ec6ba4aa2b40484d47abe89f82947ea1a8925a
Alfred <arwagaju@gmail.com>
2020-07-20 14:05:15 -0400

    added an Enrollment interface

---
+++ ClassRegistration/ClassRegistration.App/Interfaces/IEnrollmentRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassRegistration.App.Interfaces
+{
+    /// <summary>
+    /// This interface includes common functionality that is needed by the Enrollment repository
+    /// </summary>
+    public interface IEnrollmentRepo
+    {
+
+        int GetCredits(int id, string term);
+
+        void Save();
+
+
+
+    }
+}
