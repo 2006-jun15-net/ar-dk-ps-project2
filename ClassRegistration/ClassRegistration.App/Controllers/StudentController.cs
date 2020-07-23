70953e2f6d425ab1d9ab6b03170548599330eb94
Dusten Knull <dakatk97@gmail.com>
2020-07-23 10:12:05 -0500

    Removed redundancy

--- ClassRegistration/ClassRegistration.App/Controllers/StudentController.cs
+++ ClassRegistration/ClassRegistration.App/Controllers/StudentController.cs
@@ -77,18 +77,5 @@ namespace ClassRegistration.App.Controllers
 
             return Ok (totalAmount);
         }
-
-        [HttpGet ("{id}/credits_met")]
-        public async Task<IActionResult> GetCreditsRequirementsMet (int id)
-        {
-            var student = await _studentRepository.FindById (id);
-
-            if (student == default)
-            {
-                return BadRequest ();
-            }
-
-            return Ok (new { requirmentsMet = student.CreditRequirementsMet });
-        }
     }
 }
